9a988dbb3c9d21b42db1236a92e22af41dbbf54e
Kamisama <kami@kamisama.me>
2013-01-30 17:28:02 -0500

    Add new Job Status for Scheduled job

---
+++ lib/ResqueScheduler/Job/Status.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Status tracker/information for a job.
+ *
+ * @package     ResqueScheduler
+ * @subpackage  ResqueScheduler.Job
+ * @author      Wan Qi Chen <kami@kamisama.me>
+ * @copyright   Copyright 2013, Wan Qi Chen <kami@kamisama.me>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace Kamisama\ResqueScheduler\Job;
+
+class Status extends \Resque_Job_Status
+{
+    const STATUS_SCHEDULED = 63;
+}
