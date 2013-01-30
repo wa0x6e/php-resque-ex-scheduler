237690f65e7d6433cdcc96a39549a8d6c5099051
Kamisama <kami@kamisama.me>
2013-01-30 15:00:30 -0500

    Extends Resque's Worker class to reuse some method Format code to PSR-2 standard

--- lib/ResqueScheduler/Worker.php
+++ lib/ResqueScheduler/Worker.php
@@ -7,121 +7,120 @@
  * @copyright	(c) 2012 Chris Boulton
  * @license		http://www.opensource.org/licenses/mit-license.php
  */
-class ResqueScheduler_Worker
+
+namespace Kamisama\ResqueScheduler;
+
+class Worker extends \Resque_Worker
 {
-	const LOG_NONE = 0;
-	const LOG_NORMAL = 1;
-	const LOG_VERBOSE = 2;
-	
-	/**
-	 * @var int Current log level of this worker.
-	 */
-	public $logLevel = 0;
-	
-	/**
-	 * @var int Interval to sleep for between checking schedules.
-	 */
-	protected $interval = 5;
-	
-	/**
-	* The primary loop for a worker.
-	*
-	* Every $interval (seconds), the scheduled queue will be checked for jobs
-	* that should be pushed to Resque.
-	*
-	* @param int $interval How often to check schedules.
-	*/
-	public function work($interval = null)
-	{
-		if ($interval !== null) {
-			$this->interval = $interval;
-		}
 
-		$this->updateProcLine('Starting');
-		
-		while (true) {
-			$this->handleDelayedItems();
-			$this->sleep();
-		}
-	}
-	
-	/**
-	 * Handle delayed items for the next scheduled timestamp.
-	 *
-	 * Searches for any items that are due to be scheduled in Resque
-	 * and adds them to the appropriate job queue in Resque.
-	 *
-	 * @param DateTime|int $timestamp Search for any items up to this timestamp to schedule.
-	 */
-	public function handleDelayedItems($timestamp = null)
-	{
-		while (($timestamp = ResqueScheduler::nextDelayedTimestamp($timestamp)) !== false) {
-			$this->updateProcLine('Processing Delayed Items');
-			$this->enqueueDelayedItemsForTimestamp($timestamp);
-		}
-	}
-	
-	/**
-	 * Schedule all of the delayed jobs for a given timestamp.
-	 *
-	 * Searches for all items for a given timestamp, pulls them off the list of
-	 * delayed jobs and pushes them across to Resque.
-	 *
-	 * @param DateTime|int $timestamp Search for any items up to this timestamp to schedule.
-	 */
-	public function enqueueDelayedItemsForTimestamp($timestamp)
-	{
-		$item = null;
-		while ($item = ResqueScheduler::nextItemForTimestamp($timestamp)) {
-			$this->log('queueing ' . $item['class'] . ' in ' . $item['queue'] .' [delayed]');
-			
-			Resque_Event::trigger('beforeDelayedEneue', array(
-				'queue' => $item['queue'],
-				'class' => $item['class'],
-				'args'  => $item['args'],
-			));
+    /**
+     * @var int Interval to sleep for between checking schedules.
+     */
+    protected $interval = 5;
 
-			$payload = array_merge(array($item['queue'], $item['class']), $item['args']);
-			call_user_func_array('Resque::enqueue', $payload);
-		}
-	}
-	
-	/**
-	 * Sleep for the defined interval.
-	 */
-	protected function sleep()
-	{
-		sleep($this->interval);
-	}
-	
-	/**
-	 * Update the status of the current worker process.
-	 *
-	 * On supported systems (with the PECL proctitle module installed), update
-	 * the name of the currently running process to indicate the current state
-	 * of a worker.
-	 *
-	 * @param string $status The updated process title.
-	 */
-	private function updateProcLine($status)
-	{
-		if(function_exists('setproctitle')) {
-			setproctitle('resque-scheduler-' . ResqueScheduler::VERSION . ': ' . $status);
-		}
-	}
-	
-	/**
-	 * Output a given log message to STDOUT.
-	 *
-	 * @param string $message Message to output.
-	 */
-	public function log($message)
-	{
-		if($this->logLevel == self::LOG_NORMAL) {
-			fwrite(STDOUT, "*** " . $message . "\n");
-		}
-		else if($this->logLevel == self::LOG_VERBOSE) {
-			fwrite(STDOUT, "** [" . strftime('%T %Y-%m-%d') . "] " . $message . "\n");
-		}
-	}
+    /**
+    * The primary loop for a worker.
+    *
+    * Every $interval (seconds), the scheduled queue will be checked for jobs
+    * that should be pushed to Resque.
+    *
+    * @param int $interval How often to check schedules.
+    */
+    public function work($interval = null)
+    {
+        if ($interval !== null) {
+            $this->interval = $interval;
+        }
+
+        $this->updateProcLine('Starting');
+        $this->startup();
+
+        while (true) {
+            if ($this->shutdown) {
+                break;
+            }
+
+            $this->handleDelayedItems();
+            $this->sleep();
+        }
+
+        $this->unregisterWorker();
+    }
+
+    /**
+     * Handle delayed items for the next scheduled timestamp.
+     *
+     * Searches for any items that are due to be scheduled in Resque
+     * and adds them to the appropriate job queue in Resque.
+     *
+     * @param DateTime|int $timestamp Search for any items up to this timestamp to schedule.
+     */
+    public function handleDelayedItems($timestamp = null)
+    {
+        while (($timestamp = ResqueScheduler::nextDelayedTimestamp($timestamp)) !== false) {
+            $this->updateProcLine('Processing Delayed Items');
+            $this->enqueueDelayedItemsForTimestamp($timestamp);
+        }
+    }
+
+    /**
+     * Schedule all of the delayed jobs for a given timestamp.
+     *
+     * Searches for all items for a given timestamp, pulls them off the list of
+     * delayed jobs and pushes them across to Resque.
+     *
+     * @param DateTime|int $timestamp Search for any items up to this timestamp to schedule.
+     */
+    public function enqueueDelayedItemsForTimestamp($timestamp)
+    {
+        $item = null;
+        while ($item = ResqueScheduler::nextItemForTimestamp($timestamp)) {
+            $this->log(
+                array(
+                'message' => 'Moving scheduled job ' . strtoupper($item['class']) . ' to ' . strtoupper($item['queue']),
+                'data' => array(
+                    'type' => 'schedule-in',
+                    'args' => array('timestamp' => $timestamp, 'class' => $item['class'], 'queue' => $item['queue'])
+                    )
+                ),
+                self::LOG_TYPE_INFO
+            );
+            \Resque_Event::trigger(
+                'beforeDelayedEneue',
+                array(
+                    'queue' => $item['queue'],
+                    'class' => $item['class'],
+                    'args'  => $item['args'],
+                )
+            );
+
+            $payload = array_merge(array($item['queue'], $item['class']), $item['args'], array($item['track']));
+            call_user_func_array('\Resque::enqueue', $payload);
+        }
+    }
+
+    /**
+     * Sleep for the defined interval.
+     */
+    protected function sleep()
+    {
+        $this->log(array('message' => 'Sleeping for ' . $this->interval, 'data' => array('type' => 'sleep', 'second' => $this->interval)), self::LOG_TYPE_DEBUG);
+        sleep($this->interval);
+    }
+
+    /**
+     * Update the status of the current worker process.
+     *
+     * On supported systems (with the PECL proctitle module installed), update
+     * the name of the currently running process to indicate the current state
+     * of a worker.
+     *
+     * @param string $status The updated process title.
+     */
+    protected function updateProcLine($status)
+    {
+        if (function_exists('setproctitle')) {
+            setproctitle('resque-scheduler-' . ResqueScheduler::VERSION . ': ' . $status);
+        }
+    }
 }
