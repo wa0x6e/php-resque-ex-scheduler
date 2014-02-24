eeb7f0e26a836dbe3b56d0bb927f0f68bcda399d
Michael Messig <michael@messig.com>
2014-02-25 09:15:12 +1100

    Added s_time isset check for existing jobs

--- lib/ResqueScheduler/Worker.php
+++ lib/ResqueScheduler/Worker.php
@@ -86,7 +86,7 @@ class Worker extends \Resque_Worker
                         'class' => $item['class'],
                         'queue' => $item['queue'],
                         'job_id' => $item['args'][0]['id'],
-                        'wait' => round(microtime(true) - $item['s_time'], 3),
+                        'wait' => round(microtime(true) - (isset($item['s_time']) ? $item['s_time'] : 0), 3),
                         's_wait' => $timestamp - floor(isset($item['s_time']) ? $item['s_time'] : 0)
                         )
                     )
